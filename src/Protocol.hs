3fafc05d5332d5344c8bd8848a35abe73359354f
Michael Litchard <litchard.michael@gmail.com>
2023-03-03 10:24:59 -0800

    instruction JSON

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -1,5 +1,6 @@
 module Protocol where 
 
+import Data.Aeson 
 import Protocol.Protocol
 
 data Instruction 
@@ -7,3 +8,20 @@ data Instruction
       instructionAID :: Text 
     , instruction :: Either Meta Command
     } deriving (Generic,Ord,Eq, Show)
+
+instance ToJSON Instruction where 
+  toJSON (MkInstruction {..}) = 
+    object [("instruction-data")
+    .= object [("aid" .= toJSON instructionAID), "instruction" 
+         .= (either (toJSON :: Meta -> Value) 
+                    (toJSON :: Command -> Value) 
+                    instruction)]]
+
+instance FromJSON Instruction where
+    parseJSON = withObject "Instruction" $ \o -> do
+        res <- o .: "instruction-data"
+        aid <- res .: "aid"
+        instruct <- res .: "instruction"
+        instruction' 
+            <- (Left <$> parseJSON instruct) <|> (Right <$> parseJSON instruct)
+        pure $ MkInstruction aid instruction'
